997d399f6893e3c51a78068e611425d813f14bf4
lingtran <lxiutran@gmail.com>
2016-04-09 15:01:39 -0600

    Pass feature test to view playlists index

--- spec/features/user_views_all_playlists_spec.rb
+++ spec/features/user_views_all_playlists_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.feature "User can view all playlists" do
   scenario "they can see the hyperlinked name of each playlist" do
 # Given that playlists exist in the database
-  
-# When I visit the playlists index
-# Then I should see each playlist's name
-# And each name should link to that playlist's individual page
+    playlists = create_list(:playlist_with_songs, 2)
+  # When I visit the playlists index
+
+    visit playlists_path
+  # Then I should see each playlist's name
+    # And each name should link to that playlist's individual page
+
+    playlists.each do |playlist|
+      expect(page).to have_link playlist.name, href: playlist_path(playlist)
+    end
   end
 end
