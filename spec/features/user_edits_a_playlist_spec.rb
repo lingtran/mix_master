6bde5e235de3124794d11eb0c14efd3cc6fa398c
lingtran <lxiutran@gmail.com>
2016-04-09 14:21:00 -0600

    Add feature tests to view all playlists and edit a playlist

---
+++ spec/features/user_edits_a_playlist_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "User can edit a playlist" do
+  scenario " " do
+  #   As a user
+  # Given that a playlist and songs exist in the database
+  # When I visit that playlist's show page
+  # And I click on "Edit"
+  # And I enter a new playlist name
+  # And I select an additional song
+  # And I uncheck an existing song
+  # And I click on "Update Playlist"
+  # Then I should see the playlist's updated name
+  # And I should see the name of the newly added song
+  # And I should not see the name of the removed song
+  end
+end
---
+++ spec/features/user_views_all_playlists_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "User can view all playlists" do
+  scenario "they can see the hyperlinked name of each playlist" do
+# Given that playlists exist in the database
+  
+# When I visit the playlists index
+# Then I should see each playlist's name
+# And each name should link to that playlist's individual page
+  end
+end
